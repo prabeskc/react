bb2a10278f426b0e59260dc710ddeb89f2066ccd
prabeskc <kcprabesh822@gmail.com>
2024-05-19 12:38:14 +0545

    mapping

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,6 +1,29 @@
+import path from 'path'
+import { title } from 'process'
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation} from 'react-router-dom'
+
+const NavItems= [
+{
+  title:"Home",
+  path:"/"
+},
+{
+  title:"Product",
+  path:"/products"
+},
+{
+  title:"Pricing",
+  path:"/pricing"
+},
+{
+  title:"Contact",
+  path:"/contact"
+}
+]
+
 const Header = () => {
+  const location =useLocation();
   return (
     
 
@@ -17,22 +40,21 @@ const Header = () => {
         </svg>
     </button>
     <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-      <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-        <li>
-          <Link to="/" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</Link>
-        </li>
-        <li>
-          <Link to="/aboutus" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</Link>
-        </li>
-        <li>
-          <Link to="/products" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Product</Link>
-        </li>
-        <li>
-          <Link to="/pricing" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</Link>
-        </li>
-        <li>
-          <Link to="/contact" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</Link>
-        </li>
+      <ul className="flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        {
+          NavItems.map((items, idx)=>(
+            <li key={idx}>
+              <Link
+              to={items.path}
+              className={`${items.path===location.pathname ? "text-blue-700" :"text-black"} block py-2 px -3 rounded md:bg-transparent md:p-0`}
+              aria-current="page">{items.title}</Link>
+            
+    
+            </li>
+          )
+        )
+        }
+          
       </ul>
     </div>
   </div>
