1e2d5e58ac42d60fed5c17bf49d8f0c7f41804ae
prabeskc <kcprabesh822@gmail.com>
2024-06-02 12:37:44 +0545

    sliders

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,13 @@
 
 import React, { useState } from "react";
 import Carousel from "../../component/home/carousel";
+import SwiperSlider from "../../component/home/slider";
 
 
 const Home = () => {
   
   return (
-   <Carousel />
+   <SwiperSlider />
   );
 };
 
